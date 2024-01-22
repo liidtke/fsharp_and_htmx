15357fe34e189f10a6872c132f4df10a74892bee
Aldo Liidtke <aldo.liidtke@iatec.com>
2024-01-22 13:30:18 -0300

    add

--- SystemMonitor/MonitorServer.Client/Loader.fs
+++ SystemMonitor/MonitorServer.Client/Loader.fs
@@ -4,30 +4,64 @@ open Domain
 open System
 open System.Diagnostics
 
-let run name =
-    let start = ProcessStartInfo()
-    start.CreateNoWindow <- true
-    start.RedirectStandardError <- true
-    start.RedirectStandardOutput <- true
-    start.RedirectStandardInput <- true
-    start.FileName <- "/bin/bash"
-    start.Arguments <- $"-c \"{name}\""
 
-    use proc = Process.Start(start)
+module Raw =
 
-    async {
-        return! proc.StandardOutput.ReadToEndAsync() |> Async.AwaitTask
-    }
+    let run name =
+        async {
+            use p = new Process()
+            let inf = ProcessStartInfo()
+            inf.CreateNoWindow <- true
+            inf.RedirectStandardError <- true
+            inf.RedirectStandardOutput <- true
+            inf.RedirectStandardInput <- true
+            inf.FileName <- "/bin/bash"
+            inf.Arguments <- $"-c \"{name}\""
 
-let parseRawCPU raw = raw
+            let proc = Process.Start(inf)
 
-let cpuTemp: StatLoader =
-    let value = run "cat /proc/cpu" |> Async.RunSynchronously |> parseRawCPU
+            return! proc.StandardOutput.ReadToEndAsync() |> Async.AwaitTask
+        }
+
+    let getRawSensors () =
+        let value = run "sensors" |> Async.RunSynchronously
+        // printfn "%s" value
+        value
+
+    let parseRawCPUTemp (raw: string) =
+        let keyword = "Tctl"
+        let lines = raw.Split("\n")
+        // printfn "%i" lines.Length
+
+        let line = lines |> Seq.find (fun i -> i.Contains(keyword))
+        // printfn "%s" line
+
+        let plusIndex = line.IndexOf("+")
+
+        if plusIndex > -1 then
+            line[plusIndex + 1 .. plusIndex + 4]
+        else
+            raw
+
+
+    let rawCpuStat () =
+        run "mpstat -P all" |> Async.RunSynchronously
+
+    let parseRawCPUUtilization (raw: string) =
+        let values = raw.Split(" ", StringSplitOptions.RemoveEmptyEntries) |> Seq.toList
+        let index = values |> Seq.findIndex(fun i -> i = "all")
+        printf "index:%i"
+        values.[index + 1].Replace(",", ".")
+        
+
+open Raw
+
+let cpuTemp value : StatLoader =
     fun () ->
         { device = "CPU"
           name = "Generic CPU"
           stat = "Temperature"
-          value = "50" }
+          value = value }
 
 let cpuUsage: StatLoader =
     fun () ->
@@ -43,4 +77,10 @@ let memoryUsage: StatLoader =
           stat = "Utilization"
           value = "30" }
 
-let loaders: StatLoader list = [ cpuTemp; memoryUsage ]
+let loaders: StatLoader list =
+    let rawSensors = getRawSensors ()
+    let cpuValue = rawSensors |> parseRawCPUTemp
+    let cpuUtilization = rawCpuStat () |> parseRawCPUUtilization
+    printfn "%s" cpuValue
+    printfn "%s" cpuUtilization
+    [ cpuTemp cpuValue; memoryUsage ]
